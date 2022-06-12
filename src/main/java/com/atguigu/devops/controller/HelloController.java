1ff129cf6d44d31ecfb5459a0b0477eb5f109607
leifengyang <534096094@qq.com>
2022-06-12 10:45:36 +0800

    项目初始化

---
+++ src/main/java/com/atguigu/devops/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.atguigu.devops.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RestController
+public class HelloController {
+
+    @GetMapping({"/","/hello"})
+    public String sayHello(){
+
+        String format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        return "你好：现在是【"+format+"】";
+    }
+}
